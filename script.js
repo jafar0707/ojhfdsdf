63a568ad954a15598252334cc37ee31c98ee05c8
jafar0707 <jafartolibjonov58@gmail.com>
2025-05-17 18:27:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+// a.push('banan')qoshish
+// a.pop()olib tashash
+// a.shift()olib tashash boshidan
+// a.unshif('banan')qoshish boshidan
+
+let a = ['olma',' banan ','gilos ' ,'kiwi']
+
+alert(a)
+
+let b = confirm('malumot qoshasizmi?')
+
+
+if(b == true){
+    a.push('mandarin')
+    alert(a)
+}else{
+    a.pop()
+    alert(a)
+}
